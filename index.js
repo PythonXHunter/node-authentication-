bad370f63ec490150b00841ae421b4786aaeec84
PythonXHunter <aryan.computerscience@gmail.com>
2024-01-25 15:50:48 +0530

    added login

--- index.js
+++ index.js
@@ -21,26 +21,69 @@ app.use(session({
 
 const Users = [];
 
-app.get('/signup', (req, res, next) => {
+app.get('/signup', (req, res) => {
   res.render("signup");
 });
 
-app.post('/signup', (req, res, next) => {
+app.post('/signup', (req, res) => {
   if(!req.body.id || !req.body.password){
-    res.status(400).send("Invalid details!");
+    res.status(400).json({ message: "Invalid details" });
   } else {
-    Users.filter(user => {
-      if(user.id === req.body.id){
-        res.render("signup", {
+    for(let i=0;i<Users.length;i++){
+      let user = Users[i];
+      if(req.body.id === user.id){
+        return res.render('signup', {
           message: "User Already Exists! Login or choose another user id"
         });
       }
-    });
-    const newUser = { id: req.body.id, password: req.body.password };
+    }
+    const { id, password } = req.body;
+    const newUser = { id: id, password: password};
     Users.push(newUser);
     req.session.user = newUser;
-    res.redirect('/protected_page');
+    // console.log(req.session);
+    res.redirect('/login');
   }
+});
+
+function checkSignIn(req, res, next){
+  if(req.session.user){
+    next();
+  } else {
+    const err = new Error("Not logged in!");
+    next(err);
+  }
+}
+
+app.get('/protected_page', checkSignIn, (req, res) => {
+  res.render('protected_page', { id: req.session.user.id });
+});
+
+app.get('/login', (req, res) => {
+  console.log(Users);
+  res.render("login");
+});
+
+app.post('/login', (req, res) => {
+  if(!req.body.id || !req.body.password){
+    res.render("login", {
+      message: "Please enter both id and password"
+    });
+  } else {
+    Users.forEach(user => {
+      if(user.id !== req.body.id && user.password !== req.body.password){
+        req.session.user = user;
+        res.render('login', {
+          message: "Invalid credentials!"
+        });
+      }
+    });
+  }
+  res.redirect('/protected_page');
+});
+
+app.get('/signup_again', (req, res) => {
+  res.render('signup_again');
 });
 
 const port = 3000;
